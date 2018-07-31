f2d992562df2f220d2937ae1fa232d2f572c3451
nguyenduytamgithub <nguyenduytamgithub@gmail.com>
2018-07-31 16:35:39 +0700

    update toan bo

--- ANDROID/iGlobal/app/src/main/java/com/example/nguyenduytam/i_global/MainActivity.java
+++ ANDROID/iGlobal/app/src/main/java/com/example/nguyenduytam/i_global/MainActivity.java
@@ -34,8 +34,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     GoogleMap map;
     double lat = 10.762636;
     double lng = 106.681219;
+<<<<<<< HEAD
+    public static String nation = "";
+    public static String flag = "";
+    public static String capital = "";
+    public static String area = "";
+    public static String population = "";
+    public static String subregion = "";
+    public static String data = "";
+    public static String code = "";
     LatLng myLocal = new LatLng(lat, lng);
     ImageView icView;
+    private static final String TAG = "MainActivity";
+    private Address address;
+    public  static String urlCountry = "https://restcountries.eu/rest/v2/name/";
+    public  static String urlFlag = "http://www.countryflags.io/";
+    public  static String partUrlFlag = "/flat/64.png";
+=======
+    LatLng myLocal = new LatLng(lat, lng);
+    ImageView icView;
+>>>>>>> adc6d2d864a712b5d1cd1dc43346af6fbc63e25c
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,15 +62,47 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.idMap);
         mapFragment.getMapAsync(this);
         icView = (ImageView)findViewById(R.id.ic_view);
+<<<<<<< HEAD
+
+        icView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent screen_info = new Intent(MainActivity.this, man_hinh_thong_tin.class);
+                if (address != null){
+
+                    screen_info.putExtra("quocgia", nation);
+                    screen_info.putExtra("vitri", address.getFeatureName().toString());
+                    screen_info.putExtra("thudo", capital);
+                    screen_info.putExtra("dientich", area +" km²");
+                    screen_info.putExtra("khihau", subregion);
+                    screen_info.putExtra("danso", population +" người");
+                    screen_info.putExtra("gioithieu","Bài viết");
+                }else{
+                    screen_info.putExtra("quocgia", "Việt Nam");
+                    screen_info.putExtra("vitri", "University of Science");
+                    screen_info.putExtra("thudo", "Hà Nội");
+                    screen_info.putExtra("dientich", "331,210 km²");
+                    screen_info.putExtra("khihau", "Nhiệt đới gió mùa");
+                    screen_info.putExtra("danso", "92.7 triệu người");
+                    screen_info.putExtra("gioithieu","Bài viết");
+                }
+=======
         icView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent screen_info = new Intent(MainActivity.this, man_hinh_thong_tin.class);
+>>>>>>> adc6d2d864a712b5d1cd1dc43346af6fbc63e25c
                 startActivity(screen_info);
             }
         });
     }
+<<<<<<< HEAD
+
+=======
     private static final String TAG = "MainActivity";
+>>>>>>> adc6d2d864a712b5d1cd1dc43346af6fbc63e25c
     private void init(){
         Log.d(TAG, "init: initializing");
         mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -74,6 +124,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void geoLocate() {
             Log.d(TAG, "geoLocate: geoLocating");
+<<<<<<< HEAD
+        urlCountry = "https://restcountries.eu/rest/v2/name/";
+        urlFlag = "http://www.countryflags.io/";
+        nation = "";
+        flag = "";
+        capital = "";
+        area = "";
+        population = "";
+        subregion = "";
+        data = "";
+        code = "";
+=======
+>>>>>>> adc6d2d864a712b5d1cd1dc43346af6fbc63e25c
             String searchString = mSearchText.getText().toString();
         Geocoder geocoder = new Geocoder(this);
         List<Address> list = new ArrayList<>();
@@ -83,6 +146,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.e(TAG, "geoLocate: IOException: "+ e.getMessage());
         }
         if (list.size() > 0){
+<<<<<<< HEAD
+            address = list.get(0);
+           // Log.d(TAG, "geoLocate: found a location: " + address.getLongitude() + "  " + address.getLatitude());
+             Log.d(TAG, "geoLocate: found a location: " + address.toString());
+
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+            lng = address.getLongitude();
+            lat = address.getLatitude();
+            urlCountry += address.getCountryName();
+            Log.d(TAG, "geoLocate: found a location: " + lng + "  " + lat);
+            fetchData process = new fetchData();
+            process.execute();
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocal,13));
+=======
             Address address = list.get(0);
            // Log.d(TAG, "geoLocate: found a location: " + address.getLongitude() + "  " + address.getLatitude());
              Log.d(TAG, "geoLocate: found a location: " + address.getAddressLine(0).toString());
@@ -91,17 +168,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             lat = address.getLatitude();
             Log.d(TAG, "geoLocate: found a location: " + lng + "  " + lat);
 
+>>>>>>> adc6d2d864a712b5d1cd1dc43346af6fbc63e25c
             map.addMarker(new MarkerOptions().position(new LatLng(lat,lng))
                     .title(address.getAddressLine(0).toString()));
             map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lat,lng)));
         }
+<<<<<<< HEAD
+=======
 
+>>>>>>> adc6d2d864a712b5d1cd1dc43346af6fbc63e25c
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+<<<<<<< HEAD
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocal,16));
+=======
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocal,13));
+>>>>>>> adc6d2d864a712b5d1cd1dc43346af6fbc63e25c
         map.addMarker(new MarkerOptions().position(myLocal)
                 .title("University of Science"));
         map.moveCamera(CameraUpdateFactory.newLatLng(myLocal));
--- ANDROID/iGlobal/app/src/main/java/com/example/nguyenduytam/i_global/man_hinh_thong_tin.java
+++ ANDROID/iGlobal/app/src/main/java/com/example/nguyenduytam/i_global/man_hinh_thong_tin.java
@@ -3,6 +3,47 @@ package com.example.nguyenduytam.i_global;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+<<<<<<< HEAD
+import android.text.style.LeadingMarginSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.caverock.androidsvg.SVGImageView;
+import com.squareup.picasso.Picasso;
+
+public class man_hinh_thong_tin extends AppCompatActivity {
+    ImageView icBackView;
+    TextView textQgInput;
+    TextView textVtInput;
+    TextView textTdInput;
+    TextView textDsInput;
+    TextView textKhInput;
+    TextView textGtInput;
+    TextView textDtInput;
+    ImageView imageFlag;
+    LinearLayout image_from_url;
+
+    private static final String TAG = "man_hinh_2";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_thong_tin);
+        icBackView = (ImageView) findViewById(R.id.ic_back_view);
+        textQgInput = (TextView)findViewById(R.id.text_qg_input);
+        textVtInput = (TextView)findViewById(R.id.text_vt_input);
+        textDsInput = (TextView)findViewById(R.id.text_ds_input);
+        textTdInput = (TextView)findViewById(R.id.text_td_input);
+        textKhInput = (TextView)findViewById(R.id.text_kh_input);
+        textGtInput = (TextView)findViewById(R.id.text_gt_input);
+        textDtInput = (TextView)findViewById(R.id.text_dt_input);
+        imageFlag = (ImageView)findViewById(R.id.image_flag);
+
+=======
 import android.view.View;
 import android.widget.ImageView;
 
@@ -13,6 +54,7 @@ public class man_hinh_thong_tin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_hinh_thong_tin);
         icBackView = (ImageView) findViewById(R.id.ic_back_view);
+>>>>>>> adc6d2d864a712b5d1cd1dc43346af6fbc63e25c
         icBackView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -20,5 +62,41 @@ public class man_hinh_thong_tin extends AppCompatActivity {
                 startActivity(screen_main);
              }
         });
+<<<<<<< HEAD
+        Bundle bd = getIntent().getExtras();
+        if (bd != null){
+
+            String text_qg_input = bd.getString("quocgia");
+            String text_ds_input = bd.getString("danso");
+            String text_kh_input = bd.getString("khihau");
+            String text_dt_input = bd.getString("dientich");
+            String text_td_input = bd.getString("thudo");
+            String text_gt_input = bd.getString("gioithieu");
+            String text_vt_input = "Hình ảnh ";
+                    text_vt_input = text_vt_input + bd.getString("vitri");
+            Log.d(TAG, MainActivity.flag);
+
+            Picasso.with(this).load(MainActivity.urlFlag).into(imageFlag);
+            textQgInput.setText(text_qg_input);
+            textVtInput.setText(text_vt_input);
+            textDsInput.setText(text_ds_input);
+            textDtInput.setText(text_dt_input);
+            textKhInput.setText(text_kh_input);
+            textTdInput.setText(text_td_input);
+            textGtInput.setText(text_gt_input);
+
+        }else {
+            String text_qg_input = "Khong co du lieu";
+            textQgInput.setText(text_qg_input);
+            String text_vt_input = "Khong co du lieu";
+            textQgInput.setText(text_vt_input);
+        }
+
     }
+
+
+
+=======
+    }
+>>>>>>> adc6d2d864a712b5d1cd1dc43346af6fbc63e25c
 }
